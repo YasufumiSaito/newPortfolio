a8a9cacd0cf6fac9a5bf2f55654afef21ddfea1b
Yasufumi Saito <saitoyasufumi@gmail.com>
2016-01-23 02:28:07 -0500

    preloaded added

--- js/main.js
+++ js/main.js
@@ -23,27 +23,27 @@ $(function(){
 			})
 		});
 
-	// Story interactivities
-	$(function(){
-		$("#switch0").click(function(){
-			$("#photo0").toggleClass('hidden');
-		});
-		$("#switch1").click(function(){
-			$("#photo1").toggleClass('hidden');
-		});	
-		$("#switch2").click(function(){
-			$("#photo2").toggleClass('hidden');
-		});
-		$("#switch3").click(function(){
-			$("#photo3").toggleClass('hidden');
-		});
-		$("#switch4").click(function(){
-			$("#photo4").toggleClass('hidden');
-		});
-		$("#switch5").click(function(){
-			$("#photo5").toggleClass('hidden');
-		});
-		$("#switch6").click(function(){
-			$("#photo6").toggleClass('hidden');
-		});
+// Story interactivities
+$(function(){
+	$("#switch0").click(function(){
+		$("#photo0").toggleClass('hidden');
+	});
+	$("#switch1").click(function(){
+		$("#photo1").toggleClass('hidden');
 	});	
+	$("#switch2").click(function(){
+		$("#photo2").toggleClass('hidden');
+	});
+	$("#switch3").click(function(){
+		$("#photo3").toggleClass('hidden');
+	});
+	$("#switch4").click(function(){
+		$("#photo4").toggleClass('hidden');
+	});
+	$("#switch5").click(function(){
+		$("#photo5").toggleClass('hidden');
+	});
+	$("#switch6").click(function(){
+		$("#photo6").toggleClass('hidden');
+	});
+});	
